0ee76c4cb7626bc9670580121a7e2ef6f2ca8630
lit.lina <angelina.work96@gmail.com>
2024-02-21 17:25:14 +0300

    Исправила скрипт EnterTriggerComponent.

--- Assets/Scripts/Components/EnterTriggerComponent.cs
+++ Assets/Scripts/Components/EnterTriggerComponent.cs
@@ -15,7 +15,7 @@ namespace Assets.Scripts.Components
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (!other.gameObject.IsInLayer(_layer)) return;
-			if (string.IsNullOrEmpty(_tag) || !other.gameObject.CompareTag(_tag)) return;
+			if (!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return;
 
 			_action?.Invoke(other.gameObject);
 		}
