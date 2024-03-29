7e98de1445a837ecdeeff90142947cc6fa4d650f
lit.lina <angelina.work96@gmail.com>
2024-03-29 15:47:35 +0300

    Реализовала систему чек-поинтов. Героя удалила со сцен, теперь он появляется на уровне при запуске около дефолтного чек-поинта.

---
+++ Assets/Scripts/Components/LevelManagement/SetFollowComponent.cs
@@ -0,0 +1,13 @@
+using Assets.Scripts.Creatures;
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class SetFollowComponent : MonoBehaviour
+{
+	private void Start()
+	{
+		var vCamera = GetComponent<CinemachineVirtualCamera>();
+		vCamera.Follow = FindObjectOfType<Hero>().transform;
+	}
+}
--- Assets/Scripts/Model/GameSession.cs
+++ Assets/Scripts/Model/GameSession.cs
@@ -1,66 +1,113 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Model
 {
-    public class GameSession : MonoBehaviour
-    {
-        [SerializeField] private PlayerData _data;
-        public PlayerData Data => _data;
-        private PlayerData _save;
+	public class GameSession : MonoBehaviour
+	{
+		[SerializeField] private PlayerData _data;
+		[SerializeField] private string _defaultCheckPoint;
 
-        private readonly CompositeDisposable _trash = new CompositeDisposable();
-        public QuickInventoryModel QuickInventory {  get; private set; }
+		public PlayerData Data => _data;
+		private PlayerData _save;
 
-        private void Awake()
-        {
-            LoadHud();
+		private readonly CompositeDisposable _trash = new CompositeDisposable();
+		public QuickInventoryModel QuickInventory { get; private set; }
 
-            if (IsSessionExit())
-            {
-                DestroyImmediate(gameObject);
-            }
-            else
-            {
-                Save();
-                InitModels();
-                DontDestroyOnLoad(this);
-            }
-        }
+		private readonly List<string> _checkPoints = new List<string>();
+
+		private void Awake()
+		{
+			var existsSession = GetExistsSession();
+
+
+			if (existsSession != null)
+			{
+				existsSession.StartSession(_defaultCheckPoint);
+				DestroyImmediate(gameObject);
+			}
+			else
+			{
+				Save();
+				InitModels();
+				DontDestroyOnLoad(this);
+				StartSession(_defaultCheckPoint);
+			}
+		}
+
+		private void StartSession(string defaultCheckPoint)
+		{
+			SetChecked(defaultCheckPoint);
+
+			LoadHud();
+			SpawnHero();
+		}
+
+		private void SpawnHero()
+		{
+			var checkPoints = FindObjectsOfType<CheckPointComponent>();
+			var lastCheckPoint = _checkPoints.Last();
+
+			foreach (var checkPoint in checkPoints)
+			{
+				if (checkPoint.Id == lastCheckPoint)
+				{
+					checkPoint.SpawnHero();
+					break;
+				}
+
+			}
+		}
 
 		private void InitModels()
 		{
-            QuickInventory = new QuickInventoryModel(_data);
-            _trash.Retain(QuickInventory);
+			QuickInventory = new QuickInventoryModel(_data);
+			_trash.Retain(QuickInventory);
 		}
 
 		private void LoadHud()
-        {
-            SceneManager.LoadScene("Hud", LoadSceneMode.Additive);
-        }
+		{
+			SceneManager.LoadScene("Hud", LoadSceneMode.Additive);
+		}
 
-        private bool IsSessionExit()
-        {
-            var sessions = FindObjectsOfType<GameSession>();
-            foreach (var gameSession in sessions)
-            {
-                if (gameSession != this)
-                    return true;
-            }
-            return false;
-        }
+		private GameSession GetExistsSession()
+		{
+			var sessions = FindObjectsOfType<GameSession>();
+			foreach (var gameSession in sessions)
+			{
+				if (gameSession != this)
+					return gameSession;
+			}
+			return null;
+		}
 
-        public void Save()
-        {
-            _save = _data.Clone();
-        }
+		public void Save()
+		{
+			_save = _data.Clone();
+		}
 
-        public void LoadLastSave()
-        {
-            _data = _save.Clone();
+		public void LoadLastSave()
+		{
+			_data = _save.Clone();
 
 			_trash.Dispose();
 			InitModels();
+		}
+
+		public bool IsChecked(string id)
+		{
+			return _checkPoints.Contains(id);
+		}
+
+		public void SetChecked(string id)
+		{
+			if (!_checkPoints.Contains(id))
+			{
+				Save();
+				_checkPoints.Add(id);
+			}
 		}
 
 		private void OnDestroy()
