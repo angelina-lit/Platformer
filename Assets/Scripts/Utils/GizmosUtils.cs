f5a157f72b7883bcd7c9294f7ef200bf7f36d61b
lit.lina <angelina.work96@gmail.com>
2024-03-29 17:24:33 +0300

    Сцена 1. Реализовала передвигающийся бекграунд (облака), который следит за камерой и инстанциируется влево и вправо в случае, если рамки камеры выходят за границы контейнера облаков (создаются новые облака).

---
+++ Assets/Scripts/Utils/GizmosUtils.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GizmosUtils : MonoBehaviour
+{
+    public static void DrawBounds(Bounds bounds, Color color)
+    {
+        var prefColor = Gizmos.color;
+		Gizmos.color = color;
+        Gizmos.DrawLine(bounds.min, new Vector3(bounds.min.x, bounds.max.y));
+        Gizmos.DrawLine(new Vector3(bounds.min.x, bounds.max.y), bounds.max);
+		Gizmos.DrawLine(bounds.max, new Vector3(bounds.max.x, bounds.min.y));
+		Gizmos.DrawLine(new Vector3(bounds.max.x, bounds.min.y), bounds.min);
+
+		Gizmos.color = prefColor;
+	}
+}
